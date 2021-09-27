80b3a71ba5e7d7f7f0c3b826e88960225b895f21
zhxyxxx <zhxyxxx@yahoo.co.jp>
2021-09-27 20:45:11 +0900

    format scripts

--- scripts/train_21k.sh
+++ scripts/train_21k.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #$ -cwd
 #$ -l rt_F=32
-#$ -l h_rt=23:30:00
+#$ -l h_rt=72:00:00
 #$ -j y
 #$ -o output/o.$JOB_ID
 
@@ -16,18 +16,23 @@ module load cuda/10.2/10.2.89 cudnn openmpi nccl/2.7/2.7.8-1
 
 export MASTER_ADDR=$(/usr/sbin/ip a show dev bond0 | grep inet | cut -d " " -f 6 | cut -d "/" -f 1)
 
+export DATA_DIR=/groups/gcc50533/imnet/ImageNet21k
+export MODEL=tiny
+export LR=8.0e-3
+export OUT_DIR=/groups/gcc50533/check_points/tiny/i21k/pre_training
+
 export NGPUS=128
 export NUM_PROC=4
 mpirun -npernode $NUM_PROC -np $NGPUS \
-python train_without_eval.py /groups/gcc50533/imnet/ImageNet21k \
-    --model deit_base_patch16_224 --experiment pretrain_deit_base_imagenet21k_128gpus_300epochs \
-    --sched cosine_iter --epochs 300 --lr 1.0e-3 --weight-decay 0.05 \
+python train_without_eval.py $DATA_DIR \
+    --model deit_$MODEL_patch16_224 --experiment pretrain_deit_$MODEL_imagenet21k \
+    --sched cosine_iter --epochs 90 --lr $LR --weight-decay 0.05 \
     --batch-size 64 --opt adamw --num-classes 21841 \
     --warmup-epochs 5 --cooldown-epochs 0 \
     --smoothing 0.1 --drop-path 0.1 --aa rand-m9-mstd0.5-inc1 \
     --repeated-aug --mixup 0.8 --cutmix 1.0 --reprob 0.25 \
     -j 64 --eval-metric loss \
-    --hold-epochs 50 100 150 200 250 \
-    --output /groups/gcc50533/acc12016yi/pytorch-image-models/output/train \
-    --resume /groups/gcc50533/acc12016yi/pytorch-image-models/output/train/pretrain_deit_base_imagenet21k_128gpus_300epochs/last.pth.tar \
+    --hold-epochs 10 \
+    --output $OUT_DIR \
     --log-wandb
+#    --resume /groups/gcc50533/
--- scripts/train_fractal.sh
+++ scripts/train_fractal.sh
@@ -16,17 +16,27 @@ module load cuda/10.2/10.2.89 cudnn openmpi nccl/2.7/2.7.8-1
 
 export MASTER_ADDR=$(/usr/sbin/ip a show dev bond0 | grep inet | cut -d " " -f 6 | cut -d "/" -f 1)
 
+export DATA_DIR=/groups/gca50014/Fractal/edRender/x256/FractalDB-21000_PATCHGRAY
+export MODEL=tiny
+export LR=8.0e-3
+export CLASSES=21000
+export OUT_DIR=/groups/gcc50533/check_points/tiny/21k/pre_training
+export EPOCHS=90
+
 export NGPUS=128
 export NUM_PROC=4
 mpirun -npernode $NUM_PROC -np $NGPUS \
-python train_without_eval.py /groups/gcd50691/datasets/FractalDB-10k-Color \
-    --model deit_tiny_patch16_224 --experiment pretrain_deit_tiny_fractal10k \
-    --sched cosine_iter --epochs 300 --lr 8.0e-3 --weight-decay 0.05 \
-    --batch-size 64 --opt adamw --num-classes 10000 \
+python train_without_eval.py $DATA_DIR \
+    --model deit_$MODEL_patch16_224 --experiment pretrain_deit_$MODEL_fractal$CLASSES \
+    --sched cosine_iter --epochs $EPOCHS --lr $LR --weight-decay 0.05 \
+    --batch-size 64 --opt adamw --num-classes $CLASSES \
     --warmup-epochs 5 --cooldown-epochs 0 \
     --smoothing 0.1 --drop-path 0.1 --aa rand-m9-mstd0.5-inc1 \
     --repeated-aug --mixup 0.8 --cutmix 1.0 --reprob 0.25 \
     --remode pixel --interpolation bicubic --hflip 0.0 \
-    -j 128 --eval-metric loss \
-    --hold-epochs 16 32 64 128 256 \
-    --log-wandb
+    -j 64 --eval-metric loss \
+    --hold-epochs 10 \
+    --log-wandb \
+    --output $OUT_DIR 
+#    --seed 30
+#    --resume /groups/gcc50533/
